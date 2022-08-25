f3d63e0dc3061682b524a97e30d7f3949ccf87e4
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-08-25 11:22:41 +0500

    Add files via upload

---
+++ Untitled21.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#string concatination
+a="sharps"
+b="brain"
+c=a+" " +b
+print(c)
+
+
+# 
+
+# In[3]:
+
+
+age=30
+statement="My name is sharps brain . i am "+ age
+print(statement)
+
+
+# In[4]:
+
+
+#format function method:
+age=30
+statement=" my name is python , i am {}"
+print(statement.format(age))
+
+
+# In[10]:
+
+
+quantity=2
+item_no=1032
+price=103.445
+my_order="i want {0} pieces of items of item_no {1} for price{2}dollars"
+print(my_order.format(quantity,item_no,price))
+
+
+# In[ ]:
+
+
+
+
